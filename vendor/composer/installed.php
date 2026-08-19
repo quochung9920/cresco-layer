e353a0f5c51875034df0f5f7492c0b052be3e2f1
quochung9920 <quochung9920@gmail.com>
2026-08-19 16:26:37 +0700

    Add Composer autoloading and dependency management files
    - Created composer.lock to lock project dependencies.
    - Added vendor/autoload.php for autoloading classes.
    - Implemented Composer\Autoload\ClassLoader for PSR-0 and PSR-4 autoloading.
    - Introduced Composer\InstalledVersions to manage installed package versions.
    - Added LICENSE file for Composer's licensing information.
    - Generated autoload_classmap.php, autoload_namespaces.php, autoload_psr4.php, autoload_real.php, and autoload_static.php for autoloading functionality.
    - Created installed.json and installed.php to track installed packages and their versions.
    - Implemented platform_check.php to ensure PHP version compatibility.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cresco/cresco-layer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fc1836ed7f5c1890e83e3d3152119cbc96d8a713',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cresco/cresco-layer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fc1836ed7f5c1890e83e3d3152119cbc96d8a713',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
